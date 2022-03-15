a910ad02eabf22a82fdb149b49f19ea9b2b1ffcc
Joshua Leath <lawnfurniture@icloud.com>
2022-03-15 12:11:43 -0400

    added function for updating route
    Co-authored-by: Wes Anderson <wesley.herb.anderson@gmail.com>
    Co-authored-by: Trevor Kelly <trevor.daniel.kelly@gmail.com>
    Co-authored-by: Will Rossen <wor101@gmail.com>

--- autoCanarySDK/deploy.js
+++ autoCanarySDK/deploy.js
@@ -1,8 +1,8 @@
-const { AppMeshClient, CreateVirtualNodeCommand, AwsCloudMapInstanceAttribute, ServiceDiscovery } = require("@aws-sdk/client-app-mesh")
-const createVirtualNode = require('./services/createVirtualNode')
-const createService = require('./services/createService')
-const registerTaskDefinition = require('./services/registerTaskDefinition')
-// const updateRoute = require('./services/updateRoute')
+const { AppMeshClient, CreateVirtualNodeCommand, AwsCloudMapInstanceAttribute, ServiceDiscovery } = require("@aws-sdk/client-app-mesh");
+const createVirtualNode = require('./services/createVirtualNode');
+const createService = require('./services/createService');
+const registerTaskDefinition = require('./services/registerTaskDefinition');
+const updateRoute = require('./services/updateRoute');
 // const testCanary = require('./services/testCanary')
 // const deleteVirtualNode = require('./services/deleteVirtualNode')
 // const updateService = require('./services/updateService')
@@ -16,4 +16,15 @@ const taskName = `${chimeraConfig.meshName}-${chimeraConfig.serviceName}-${chime
 
 // const virtualNode = createVirtualNode(chimeraConfig, virtualNodeName, taskName);
 // const taskDef = registerTaskDefinition(chimeraConfig, taskName, virtualNodeName);
-const service = createService(chimeraConfig, taskName, virtualNodeName);
+// const service = createService(chimeraConfig, taskName, virtualNodeName);
+const weightedTargets = [
+  {
+    virtualNode: chimeraConfig.originalNodeName,
+    weight: 50,
+  },
+  {
+    virtualNode: virtualNodeName,
+    weight: 50,
+  }
+];
+updateRoute(chimeraConfig, weightedTargets,);
