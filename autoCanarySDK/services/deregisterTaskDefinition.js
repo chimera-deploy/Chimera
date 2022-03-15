5fe2174ec7b1c4fbc15ab46800b3e642148b5c7e
Joshua Leath <lawnfurniture@icloud.com>
2022-03-15 14:14:29 -0400

    implemented tear down functions
    Co-authored-by: Wes Anderson <wesley.herb.anderson@gmail.com>
    Co-authored-by: Trevor Kelly <trevor.daniel.kelly@gmail.com>
    Co-authored-by: Will Rossen <wor101@gmail.com>

--- autoCanarySDK/services/deregisterTaskDefinition.js
+++ autoCanarySDK/services/deregisterTaskDefinition.js
@@ -1,27 +1,24 @@
-const { ECSClient, DeregisterTaskDefinitionCommand } = require("@aws-sdk/client-ecs")
+const { ECSClient, DeregisterTaskDefinitionCommand } = require("@aws-sdk/client-ecs");
 
-const deregisterTaskDefinition = async (clientConfig, taskDefinitionWithRevision) => {
-  const client = new ECSClient(clientConfig);
+const deregisterTaskDefinition = async (taskDefinitionName) => {
+  const client = new ECSClient();
   const deregisterTaskDefinitionCommandInput = {
-    taskDefinition: taskDefinitionWithRevision,
-  }
+    taskDefinition: taskDefinitionName,
+  };
 
   const command = new DeregisterTaskDefinitionCommand(deregisterTaskDefinitionCommandInput);
   
   try {
     const response = await client.send(command)
-    console.log(`Success deregistering Task Definition named ${taskDefinition}`)
+    console.log(`Success deregistering Task Definition named ${taskDefinitionName}`)
     console.log(response)
     return response
   } catch(err) {
-    console.log(`ERROR deregistering Task Definition named ${taskDefinition}`)
+    console.log(`ERROR deregistering Task Definition named ${taskDefinitionName}`)
     console.log(err)
     return err
   }
   
 }
-// const taskDefinitionWithRevision = 'APP-serviceAv3:1'
 
-// deregisterTaskDefinition({ region: 'us-west-2'}, taskDefinitionWithRevision)
-
-module.exports = deregisterTaskDefinition
+module.exports = deregisterTaskDefinition;
--- autoCanarySDK/services/updateService.js
+++ autoCanarySDK/services/updateService.js
@@ -1,24 +1,24 @@
-const { ECSClient, UpdateServiceCommand } = require("@aws-sdk/client-ecs")
+const { ECSClient, UpdateServiceCommand } = require("@aws-sdk/client-ecs");
 
-const updateService = async (clientConfig, cluster, desiredCount, service) => {
-  const client = new ECSClient(clientConfig);
+const updateECSService = async (chimeraConfig, desiredCount, ecsServiceName) => {
+  const client = new ECSClient();
   const updateServiceCommandInput = {
-    cluster,
+    cluster: chimeraConfig.clusterName,
     desiredCount,
-    service,
-  }
+    service: ecsServiceName,
+  };
   const command = new UpdateServiceCommand(updateServiceCommandInput);
   
   try {
     const response = await client.send(command)
-    console.log(`Success updating service named ${service}`)
-    console.log(response)
-    return response
+    console.log(`Success updating service named ${ecsServiceName}`);
+    console.log(response);
+    return response;
   } catch(err) {
-    console.log(`ERROR updating service named ${service}`)
-    console.log(err)
-    return err
+    console.log(`ERROR updating service named ${ecsServiceName}`);
+    console.log(err);
+    return err;
   }
 }
 
-module.exports = updateService
+module.exports = updateECSService;
