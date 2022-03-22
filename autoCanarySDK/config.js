97f0ebde9fda415c5c7dfb284db26cff6b3e5315
Will <wor101@gmail.com>
2022-03-22 16:43:21 -0400

    tested happy and unhappy paths succesfully
    Co-authored-by: Wes Anderson <wesley.herb.anderson@gmail.com>
    Co-authored-by: Trevor Kelly <trevor.daniel.kelly@gmail.com>
    Co-authored-by: Josh Leath <joshua.leath@icloud.com>

--- autoCanarySDK/config.js
+++ autoCanarySDK/config.js
@@ -1,9 +1,9 @@
 const clientConfiguration = {
   "meshName": "chimera",
-  "newVersionNumber": "12",
-  "serviceName": "movieselector-1",
+  "newVersionNumber": "3",
+  "serviceName": "movieselector",
   "containerName": "app",
-  "imageURL": '822180497458.dkr.ecr.us-west-2.amazonaws.com/movieselector:6.0',
+  "imageURL": '822180497458.dkr.ecr.us-west-2.amazonaws.com/movieselector:5.0',
   "clusterName": "chimera",
   "serviceDiscoveryID": "srv-cz3dj4ut3k5x3ru3",
   "originalNodeName": "movieselector-1",
@@ -14,8 +14,7 @@ const clientConfiguration = {
   "envoyContainerName": "envoy",
   "region": "us-west-2",
   "awsAccountID": "822180497458",
-  "logGroup": "chimera-log-group",
-  "metricNamespace": "chimera-prometheus-metrics-2",
+  "metricNamespace": "chimera-prometheus-metrics-3",
   "cwECSSecurityGroups": ["sg-0ba88d5feee9fedc3"],
   "cwECSPrimarySubnets": ["subnet-01683c903501dfd39", "subnet-01b67f4aa9f58abbb"]
 };
--- autoCanarySDK/services/CloudWatch.js
+++ autoCanarySDK/services/CloudWatch.js
@@ -47,11 +47,8 @@ const getMetricData = async (StartTime, EndTime, metricNamespace, clusterName, t
 const getHealthCheck = async (failureThresholdTime, metricNamespace, clusterName, taskName) => {
   const millisecondsNow = Date.now();
   const now = new Date(millisecondsNow);
-  console.log("now:", now);
   const start = new Date(millisecondsNow - (failureThresholdTime * 2));
-  console.log("start:", start);
   const response = await getMetricData(start, now, metricNamespace, clusterName, taskName);
-  console.log("response:", response);
   const values = response.MetricDataResults[0].Values;
   console.log("values downstream/ingress 500:", values);
   if (values.reduce((a, v) => a + v, 0) > 0) { // hard coded for testing
