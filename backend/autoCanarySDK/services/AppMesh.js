be4cdfbb9345d94d39528c3f99565d8fd9a7121b
Trevor Kelly <Trevor.daniel.kelly@gmail.com>
2022-03-24 17:01:52 -0400

    routeNames, routerNames, and routersWithRoutes functions added to AppMesh
    Co-authored-by: Wes Anderson <wesley.herb.anderson@gmail.com>
    Co-authored-by: Trevor Kelly <trevor.daniel.kelly@gmail.com>
    Co-authored-by: Josh Leath <joshua.leath@icloud.com>

--- backend/autoCanarySDK/services/AppMesh.js
+++ backend/autoCanarySDK/services/AppMesh.js
@@ -1,4 +1,4 @@
-const { AppMeshClient, ListVirtualNodesCommand } = require("@aws-sdk/client-app-mesh");
+const { AppMeshClient, ListVirtualNodesCommand, ListVirtualRoutersCommand, ListRoutesCommand } = require("@aws-sdk/client-app-mesh");
 const { builtinModules } = require("module");
 
 const AppMesh = {
@@ -13,6 +13,56 @@ const AppMesh = {
     const response = await client.send(command);
     const nodes = response.virtualNodes;
     return nodes.map(node => node.virtualNodeName);
+  },
+
+  async routersWithRoutes(meshName) {
+    const routers = await this.routerNames(meshName);
+    const promises = [];
+    for (let i = 0; i < routers.length; i++) {
+      const p = new Promise(async (resolve, reject) => {
+        try {
+          const routes = await this.routeNames(meshName, routers[i]);
+          routers[i] = {
+            name: routers[i],
+            routes,
+          };
+          resolve();
+        } catch (e) {
+          reject(e);
+        }
+      });
+      promises.push(p);
+    }
+
+    await Promise.all(promises);
+    return routers;
+  },
+
+  async routeNames(meshName, virtualRouterName) {
+    const input = {
+      meshName,
+      virtualRouterName,
+    };
+
+    const command = new ListRoutesCommand(input);
+    const client = new AppMeshClient();
+
+    const response = await client.send(command);
+    const routes = response.routes
+    return routes.map(route => route.routeName);
+  },
+
+  async routerNames(meshName) {
+    const input = {
+      meshName,
+    }
+
+    const command = new ListVirtualRoutersCommand(input);
+    const client = new AppMeshClient();
+
+    const response = await client.send(command);
+    const routers =  response.virtualRouters;
+    return routers.map(router => router.virtualRouterName)
   }
 }
 
