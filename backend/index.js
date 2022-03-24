5fb9b0b4d60a64cef83162a14827e6c04a380579
Trevor Kelly <Trevor.daniel.kelly@gmail.com>
2022-03-24 15:56:03 -0400

    created mesh details route in index.js
    Co-authored-by: Wes Anderson <wesley.herb.anderson@gmail.com>
    Co-authored-by: Trevor Kelly <trevor.daniel.kelly@gmail.com>
    Co-authored-by: Josh Leath <joshua.leath@icloud.com>

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const Chimera = require("./autoCanarySDK/chimera");
+const AppMesh = require("./autoCanarySDK/services/AppMesh");
 const express = require("express");
 const app = express();
 app.use(express.json());
@@ -14,6 +15,20 @@ app.post('/deploy', (request, response) => {
   console.log(config);
   Chimera.deploy(config);
   response.status(200).send();
+});
+
+app.get('/mesh-details', async (request, response) => {
+  try {
+    const meshName = request.body.meshName;
+
+    const nodes = await AppMesh.nodeNames(meshName);
+    const routers = await AppMesh.routersWithRoutes(meshName);
+
+    response.status(200).json({ nodes, routers });
+  } catch (error) {
+    console.log("Error getting mesh details", error);
+    response.status(500).json({ error });
+  }
 });
 
 app.listen(PORT, () => console.log(`Backend listening on port ${PORT}`));
