e615850150ef510cec17fd9f50ad535651ef3f26
Joshua Leath <lawnfurniture@icloud.com>
2022-03-24 16:10:16 -0400

    implemented GET /ecs-details endpoint
    Co-authored-by: Will Rossen <wor101@gmail.com>
    Co-authored-by: Wes Anderson <wesley.herb.anderson@gmail.com>
    Co-authored-by: Trevor Kelly <trevor.daniel.kelly@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
 const Chimera = require("./autoCanarySDK/chimera");
+const ECSService = require("./autoCanarySDK/services/ECSService");
+const TaskDefinition = require("./autoCanarySDK/services/TaskDefinition");
 const express = require("express");
 const app = express();
 app.use(express.json());
@@ -14,6 +16,31 @@ app.post('/deploy', (request, response) => {
   console.log(config);
   Chimera.deploy(config);
   response.status(200).send();
+});
+
+const getIDFromArn = arn => {
+  const parts = arn.split('/');
+  return parts[parts.length - 1];
+};
+
+app.get('/ecs-details', async (request, response) => {
+  const { originalECSServiceName, clusterName } = request.body;
+  try {
+    const service = await ECSService.describe(clusterName, originalECSServiceName);
+    const serviceRegistryIds = service.serviceRegistries.map(registry => getIDFromArn(registry.registryArn));
+    const taskDefinitionWithRevision = getIDFromArn(service.taskDefinition);
+    const taskDefinition = await TaskDefinition.describe(taskDefinitionWithRevision);
+    const containerNames = taskDefinition.containerDefinitions.map(def => def.name);
+    response.status(200).json({
+      serviceRegistryIds,
+      taskDefinitionWithRevision,
+      containerNames
+    });
+  } catch (err) {
+    response.status(404).json(
+      { error: `unable to find service with name ${originalECSServiceName} on cluster ${clusterName}`}
+    );
+  }
 });
 
 app.listen(PORT, () => console.log(`Backend listening on port ${PORT}`));
