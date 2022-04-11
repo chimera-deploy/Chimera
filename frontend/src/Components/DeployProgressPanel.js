a389d65bb38aad40bda132a67e62aeb48c2f026b
Joshua Leath <lawnfurniture@icloud.com>
2022-04-11 16:25:06 -0400

    fixed merge bugs

--- frontend/src/Components/DeployProgressPanel.js
+++ frontend/src/Components/DeployProgressPanel.js
@@ -6,7 +6,7 @@ import AbortButton from './AbortButton';
 const EventLine = ({ event, isCurrentStage }) => {
   return (
     <div className="status-update-line">
-      <p>{event.message}</p>
+      <p>{event}</p>
       {isCurrentStage ? <img id="loading-gif" src="../../loading.gif" alt="loading gif" /> : null}
     </div>
   );
@@ -15,11 +15,10 @@ const EventLine = ({ event, isCurrentStage }) => {
 const DeployProgressPanel = () => {
   const dispatch = useDispatch();
   const [ listening, setListening ] = useState(false);
-  const [ canaryWeight, setCanaryWeight ] = useState(.75);
-  const [ stableWeight, setStableWeight] = useState(.25);
-  const { events, metrics } = useSelector(state => state.events);
+  const [ weights, setWeights ] = useState({stable: 100, canary: 0});
+  const [ events, setEvents ] = useState([]);
+  const [ metricsWidget, setMetricsWidget ] = useState('');
   const { originalECSServiceName, newECSServiceName} = useSelector(state => state.deploy);
-  console.log(metrics);
 
   useEffect(() => {
     if (!listening) {
@@ -28,28 +27,22 @@ const DeployProgressPanel = () => {
       eventListener.onmessage = (event) => {
         console.log(event);
         const data = JSON.parse(event.data);
-        const events = data.events
+        const events = data.events;
 
-        if (data.metricsWidget !== "") {
-          dispatch({ type: 'METRICS_RECEIVED', payload: data.metricsWidget });
-        } else {
-          dispatch({ type: 'EVENT_RECEIVED', payload: data.events});
+        if (data.events) {
+          setEvents(data.events);
+        }
+        if (data.metricsWidget) {
+          setMetricsWidget(data.metricsWidget);
+        }
+        if (data.weights) {
+          setWeights(data.weights);
         }
         if (events[events.length - 1] === 'closing connection') {
           eventListener.close();
         }
       }
       
-      // let eventCount = 0;
-      // const tick = () => {
-      //   if (eventCount < 10) {
-      //     console.log(events);
-      //     dispatch({ type: 'EVENT_RECEIVED', payload: events.concat({ message: `this is event ${eventCount}` }) });
-      //     eventCount++;
-      //     setTimeout(tick, 5000);
-      //   }
-      // }
-      // setTimeout(tick, 5000);
       setListening(true);
     }
   }, [listening, dispatch, events]);
@@ -59,35 +52,36 @@ const DeployProgressPanel = () => {
     <>
       <div className="row">
         <div className="col-50 no-border service-container">
-          <div className="service-box original-service" style={{backgroundColor: `rgba(100, 149, 237, ${stableWeight})`}}>
+          <div className="service-box original-service" style={{backgroundColor: `rgba(100, 149, 237, ${weights.stable/100})`}}>
             <p><strong>Stable</strong></p>
             <p><em>{originalECSServiceName}</em></p>
-            <p className="traffic-weight-text">{`${stableWeight*100}%`}</p>
+            <p className="traffic-weight-text">{`${weights.stable}%`}</p>
           </div>
-          <div className="service-box canary-service" style={{backgroundColor: `rgba(34, 139, 34, ${canaryWeight})`}}>
+          <div className="service-box canary-service" style={{backgroundColor: `rgba(34, 139, 34, ${weights.canary/100})`}}>
             <p><strong>Canary</strong></p>
             <p><em>{newECSServiceName}</em></p>
-            <p className="traffic-weight-text">{`${canaryWeight*100}%`}</p>
+            <p className="traffic-weight-text">{`${weights.canary}%`}</p>
           </div>
         </div>
         <div className="col-50 no-border action-button-container">
             {
               events.length !== 0
-                ? <EventLine event={events[events.length - 1]} isCurrentStage={events[events.length - 1].message !== 'closing connection'} />
+                ? <EventLine event={events[events.length - 1]} isCurrentStage={events[events.length - 1] !== 'closing connection'} />
                 : null
             }
           <AbortButton />
         </div>
       </div>
       {
-        metrics !== ''
-        ? <div className="row">
-            <div className="col-100">
-              <img width="1200" height="600" src={`data:image/png;base64,${metrics}`} alt='widget' />
-            </div>
+        <div className="row">
+          <div className="col-100">
+            {
+              metricsWidget !== ''
+                ? <img width="100%" src={`data:image/png;base64,${metricsWidget}`} alt='widget' />
+                : <p>Waiting for metrics...</p>
+            }
           </div>
-          
-        : null
+        </div>
       }
     </>
   );
